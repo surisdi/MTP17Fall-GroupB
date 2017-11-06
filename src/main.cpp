ed0ad72505fdddd998ad672f5610d1285aacfeaa
Didac Suris <didac8989@gmail.com>
2017-11-06 18:38:06 +0100

    Quick test files
    It works. Basic version

--- src/main.cpp
+++ src/main.cpp
@@ -7,51 +7,49 @@
 #include "Compressor.hpp"
 #include "Socket.hpp"
 
+#include "utils.hpp"
+
 using namespace std;
+
 
 int main(int argc, char* argv[])
 {
-	if(argc < 2){
-		cout << "Please provide arguments " << endl;
-		return -1;
-	}
+    const int CODE_L = 32;
+    const int REDUNDANCY = 2;
 
-	Compressor1 compressor(0.8, 5);
-	EncoderRS<32, 6> encoder;
-
-	if(string(argv[1]) == "tx") {
-
-		int bufferSize = 100;
-		bool isServer = false;
-		char ip[] = "localhost";
-
-		SocketRadio socket;
-
-		StopWait protocol(&compressor, &encoder, &socket);
-
-		char* file_name;
-		if(argc > 2){
-			file_name = argv[2];
-		}
-		else{
-			file_name = (char *)"file.txt";
-		}
-
-		std::cout << "Input file name: " << file_name << std::endl;
-
-		int sent_ok = protocol.send_text(file_name);
-
-	}else { // rx
-
-		int bufferSize = 100;
-		bool isServer = true;
-		char ip[] = "localhost";
-
-		SocketTCP socket(ip, isServer);
-		StopWait protocol(&compressor, &encoder, &socket);
-
-		int received_ok = protocol.receive_text();
-	}
-
-	return 0;
+    if(argc < 2){
+        cout << "Please provide arguments " << endl;
+        return -1;
+    }
+    
+    //TODO: Aixo no fa res, s'ha d'eliminar, només es una clase de prints
+    Compressor1 compressor(0.5, 5);
+    EncoderRS<CODE_L, REDUNDANCY> encoder;
+    
+    if(string(argv[1]) == "tx") {
+        
+        SocketRadio socket(1);
+        StopWait protocol(&compressor, &encoder, &socket);
+        
+        char* file_name;
+        if(argc > 2){
+            file_name = argv[2];
+        }
+        else{
+            file_name = (char *)"file.txt";
+        }
+        
+        std::cout << "Input file name: " << file_name << std::endl;
+        
+        int sent_ok = protocol.send_text(file_name);
+        
+    } else {
+        
+        SocketRadio socket(0);
+        StopWait protocol(&compressor, &encoder, &socket);
+        
+        int received_ok = protocol.receive_text();
+    }
+    
+    return 0;
 }
